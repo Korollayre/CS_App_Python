4b890b62ed290fbfe5efb2cab811a98084c12171
Mikhail <mbuinichienko@mail.ru>
2021-10-30 13:17:43 +0300

    Did tests for server.py file and add in server.py check for PORT in message

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import sys
 import json
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, RESPONDEFAULT_IP_ADDRESSSE
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, RESPONDEFAULT_IP_ADDRESSSE, PORT
 from common.utils import get_message, send_message
 
 
@@ -18,7 +18,7 @@ def process_user_message(message):
     :return:
     """
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+            and USER in message and PORT in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
     return {
         RESPONDEFAULT_IP_ADDRESSSE: 400,
