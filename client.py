0e0cee1179a71bfc701698a9ead17c81eb9b0053
Mikhail <mbuinichienko@mail.ru>
2021-10-31 14:01:40 +0300

    Did HW for the 5th lesson

--- client.py
+++ client.py
@@ -4,9 +4,14 @@ import sys
 import json
 import socket
 import time
+import logging
+
 from common.variables import ACTION, PRESENCE, PORT, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import get_message, send_message
+from errors import ReqFieldMissingError
+
+CLIENT_LOGGER = logging.getLogger('client')
 
 
 def user_request(port, user_name='Guest'):
@@ -24,6 +29,7 @@ def user_request(port, user_name='Guest'):
             ACCOUNT_NAME: user_name
         }
     }
+    CLIENT_LOGGER.info(f'Генерация запроса {PRESENCE} пользователя {user_name}')
     return user_data
 
 
@@ -34,11 +40,12 @@ def server_response(response):
 
     :return:
     """
+    CLIENT_LOGGER.info(f'Принят ответ сервера')
     if RESPONSE in response:
         if response[RESPONSE] == 200:
             return '200 : OK'
         return f'400 : {response[ERROR]}'
-    raise ValueError
+    raise ReqFieldMissingError(RESPONSE)
 
 
 def main():
@@ -49,11 +56,15 @@ def main():
         if request_port < 1024 or request_port > 65535:
             raise ValueError
     except IndexError:
+        CLIENT_LOGGER.error(f'Адрес и(или) порт не переданы, значения установлены по умолчанию')
         request_address = DEFAULT_IP_ADDRESS
         request_port = DEFAULT_PORT
     except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        CLIENT_LOGGER.critical(f'Указано неверное значение порта - {request_port}. В качестве порта может быть '
+                               f'указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
+
+    CLIENT_LOGGER.info(f'Запущен клиент со следующими параметрами: адрес - {request_address}, адрес - {request_port}')
 
     # Инициализация сокета и обмен
 
@@ -63,9 +74,15 @@ def main():
     send_message(transport, user_message)
     try:
         response = server_response(get_message(transport))
+        CLIENT_LOGGER.info(f'Ответ сервера принят: {response}')
         print(response)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать сообщение сервера.')
+    except ReqFieldMissingError as missing_error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{missing_error.missing_field}')
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical('Отказ узла в попытке соединения')
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -3,9 +3,15 @@
 import socket
 import sys
 import json
+import logging
+import logs.server_log_config
+
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
     PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, RESPONDEFAULT_IP_ADDRESSSE, PORT
 from common.utils import get_message, send_message
+from errors import IncorrectDataRecivedError
+
+SERVER_LOGGER = logging.getLogger('server')
 
 
 def process_user_message(message):
@@ -17,6 +23,7 @@ def process_user_message(message):
     :param message:
     :return:
     """
+    SERVER_LOGGER.info(f'Разбор сообщения {message}.')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and PORT in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
@@ -42,11 +49,11 @@ def main():
         if listen_port < 1024 or listen_port > 65535:
             raise ValueError
     except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
+        SERVER_LOGGER.critical(f'После параметра -\'p\' отсутствует номер порта.')
         sys.exit(1)
     except ValueError:
-        print(
-            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        SERVER_LOGGER.critical(f'Указано неверное значение порта - {listen_port}. В качестве порта может быть '
+                               f'указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
 
     # Затем загружаем какой адрес слушать
@@ -58,29 +65,36 @@ def main():
             listen_address = ''
 
     except IndexError:
-        print(
-            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        SERVER_LOGGER.critical(f'После параметра -\'a\' отсутствует адрес.')
         sys.exit(1)
 
+    SERVER_LOGGER.info(f'Сервер запущен. Порт для подключения: {listen_port}, адрес: {listen_address}. '
+                       f'При отсутствии адреса сервер принимает соединения со любых адресов')
     # Готовим сокет
-
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
 
-    # Слушаем порт
+    SERVER_LOGGER.info(f'Настройка сокета завершена.')
 
+    # Слушаем порт
     transport.listen(MAX_CONNECTIONS)
 
     while True:
         user, user_address = transport.accept()
+        SERVER_LOGGER.info(f'Соединение с  {user_address} установлено')
         try:
             message_from_user = get_message(user)
-            print(message_from_user)
+            SERVER_LOGGER.info(f'Получено сообщение клиента {user_address} - {message_from_user}')
             response = process_user_message(message_from_user)
+            SERVER_LOGGER.info(f'Сформирован ответ клиенту {user_address}')
             send_message(user, response)
+            SERVER_LOGGER.info(f'Ответ {response} клиенту {user_address} отправлен. Закрытие соединения.')
             user.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать сообщение клиента {user_address}. Закрытие соединения.')
+            user.close()
+        except IncorrectDataRecivedError:
+            SERVER_LOGGER.error(f'Клиент {user_address} отправил некорректные данные. Закрытие соединения.')
             user.close()
 
 
